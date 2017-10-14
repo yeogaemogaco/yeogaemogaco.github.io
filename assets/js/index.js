d309154609a5c0e8e839beac9559b3e9ae0774f2
Kayla <kaylabaek@coupang.com>
2017-10-14 18:56:39 +0900

    [CSS] 이미지 caption추가

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+/* global window */
+(function (window, document, $) {
+    'use strict';
+
+    $(function () {
+        // turn img alt into caption
+        $('.content .post > p > img[alt]').replaceWith(function () {
+            return '<figure>'
+                + '<a href="' + $(this).attr('src') + '" class="mg-link">'
+                + '<img src="' + $(this).attr('src') + '"/></a>'
+                + '<figcaption class="caption">' + $(this).attr('alt') + '</figcaption>'
+                + '</figure>';
+        });
+        // and connect magnific popup image viewer
+        // $('#post-content .mg-link').magnificPopup({
+        //     type: 'image',
+        //     closeOnContentClick: true
+        // });
+    });
+}(window, window.document, window.jQuery));
